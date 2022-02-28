201c3bcafd0f8bc561d94c9eb26aee56f685e0ec
Lucas Silva <lucassilva@agrfamosa.com.br>
2022-02-28 20:56:16 -0300

    ***

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# projeto-uptech
+<h1 align="center">Projeto Uptech</h1>
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0;
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/lucasilvadrm" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About
+
 Projeto desenvolvido como forma de avaliação.
+
+## Screenshots
+
+<img src='./screenshots/screen1.png' />
+
+## :rocket: Technologies
+
+The following tools were used in this project:
+
+- [HTML](https://devdocs.io/html/)
+- [CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+- [Javascript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+
+## :white_check_mark: Requirements
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com)
+
+## :checkered_flag: Starting
+
+bash
+# Clone this project
+$ git clone https://github.com/lucasilvadrm/projeto-uptech
+
+# Access
+$ cd projeto-uptech
+
+# Open in your favorite browser
+
+
+Made with :heart: by <a href="https://github.com/lucasilvadrm" target="_blank">Lucas Silva</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
--- public/js/script.js
+++ public/js/script.js
@@ -9,195 +9,195 @@ const shelfLife = document.getElementById('shelf-life')
 
 function liveValidation() {
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        // console.log(fullname)
-        console.log(cardFlag.value)
-        verifyInputs()
-    })
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    // console.log(fullname)
+    console.log(cardFlag.value)
+    // verifyInputs()
+  })
 
-    fullname.addEventListener('input', e => {
-        e.preventDefault()
-        const fullnameValue = fullname.value.trim();
-        if (fullnameValue === '') {
-            errorValidation(fullname, 'Campo obrigatório!')
-        } else {
-            successValidation(fullname)
-        }
-    })
-    cpf.addEventListener('input', e => {
-        e.preventDefault()
-        const cpfValue = cpf.value.trim();
-        if (cpfValue === '') {
-            errorValidation(cpf, 'Campo obrigatório!')
-        } else if (cpfValue.length < 14) {
-            errorValidation(cpf, 'CPF Inválido!')
-        } else {
-            successValidation(cpf)
-        }
-    })
+  fullname.addEventListener('input', e => {
+    e.preventDefault()
+    const fullnameValue = fullname.value.trim();
+    if (fullnameValue === '') {
+      errorValidation(fullname, 'Campo obrigatório!')
+    } else {
+      successValidation(fullname)
+    }
+  })
+  cpf.addEventListener('input', e => {
+    e.preventDefault()
+    const cpfValue = cpf.value.trim();
+    if (cpfValue === '') {
+      errorValidation(cpf, 'Campo obrigatório!')
+    } else if (cpfValue.length < 14) {
+      errorValidation(cpf, 'CPF Inválido!')
+    } else {
+      successValidation(cpf)
+    }
+  })
 
-    email.addEventListener('input', e => {
-        e.preventDefault()
-        const emailValue = email.value.trim();
-        console.log(emailValue)
-        if (emailValue === '') {
-            errorValidation(email, 'Campo obrigatório!')
-        } else if (emailValue.indexOf('@') === -1 || emailValue.indexOf('.') === -1) {
-            errorValidation(email, 'Email inválido!')
-        } else {
-            successValidation(email)
-        }
-    })
+  email.addEventListener('input', e => {
+    e.preventDefault()
+    const emailValue = email.value.trim();
+    console.log(emailValue)
+    if (emailValue === '') {
+      errorValidation(email, 'Campo obrigatório!')
+    } else if (emailValue.indexOf('@') === -1 || emailValue.indexOf('.') === -1) {
+      errorValidation(email, 'Email inválido!')
+    } else {
+      successValidation(email)
+    }
+  })
 
-    numbercvv.addEventListener('input', e => {
-        e.preventDefault()
-        const numbercvvValue = numbercvv.value.trim();
-        if (numbercvvValue === '') {
-            errorValidation(numbercvv, 'Campo obrigatório!')
-        } else if (numbercvvValue.length < 3) {
-            errorValidation(numbercvv, 'Padrão inválido para o CVV!')
-        } else {
-            successValidation(numbercvv)
-        }
-    })
+  numbercvv.addEventListener('input', e => {
+    e.preventDefault()
+    const numbercvvValue = numbercvv.value.trim();
+    if (numbercvvValue === '') {
+      errorValidation(numbercvv, 'Campo obrigatório!')
+    } else if (numbercvvValue.length < 3) {
+      errorValidation(numbercvv, 'Padrão inválido para o CVV!')
+    } else {
+      successValidation(numbercvv)
+    }
+  })
 
-    cardFlag.addEventListener('input', e => {
-        e.preventDefault()
-        const cardFlagValue = cardFlag.value.trim();
-        if (cardFlagValue === 'initial') {
-            errorValidation(cardFlag, 'Selecione uma bandeira!')
-        } else {
-            successValidation(cardFlag)
-        }
-    })
+  cardFlag.addEventListener('input', e => {
+    e.preventDefault()
+    const cardFlagValue = cardFlag.value.trim();
+    if (cardFlagValue === 'initial') {
+      errorValidation(cardFlag, 'Selecione uma bandeira!')
+    } else {
+      successValidation(cardFlag)
+    }
+  })
 
-    shelfLife.addEventListener('input', e => {
-        e.preventDefault()
-        const shelfLifeValue = shelfLife.value.trim();
-        if (shelfLifeValue === '') {
-            errorValidation(shelfLife, 'Campo obrigatório!')
-        } else if (shelfLifeValue.length < 7) {
-            errorValidation(shelfLife, 'Informe o mês e o ano corretamente.')
-        } else {
-            successValidation(shelfLife)
-        }
-    })
+  shelfLife.addEventListener('input', e => {
+    e.preventDefault()
+    const shelfLifeValue = shelfLife.value.trim();
+    if (shelfLifeValue === '') {
+      errorValidation(shelfLife, 'Campo obrigatório!')
+    } else if (shelfLifeValue.length < 7) {
+      errorValidation(shelfLife, 'Informe o mês e o ano corretamente.')
+    } else {
+      successValidation(shelfLife)
+    }
+  })
 }
 
 liveValidation()
 
 function verifyInputs() {
-    const fullnameValue = fullname.value.trim();
-    // console.log(fullnameValue)
-    const emailValue = email.value.trim();
-    const cpfValue = cpf.value.trim();
-    const numbercvvValue = numbercvv.value.trim();
-    const cardFlagValue = cardFlag.value.trim();
-    const shelfLifeValue = shelfLife.value.trim();
+  const fullnameValue = fullname.value.trim();
+  // console.log(fullnameValue)
+  const emailValue = email.value.trim();
+  const cpfValue = cpf.value.trim();
+  const numbercvvValue = numbercvv.value.trim();
+  const cardFlagValue = cardFlag.value.trim();
+  const shelfLifeValue = shelfLife.value.trim();
 
-    if (fullnameValue === '') {
-        errorValidation(fullname, 'Campo obrigatório!')
-    } else {
-        successValidation(fullname)
-    }
+  if (fullnameValue === '') {
+    errorValidation(fullname, 'Campo obrigatório!')
+  } else {
+    successValidation(fullname)
+  }
 
-    if (emailValue === '') {
-        errorValidation(email, 'Campo obrigatório!')
-    } else {
-        successValidation(email)
-    }
+  if (emailValue === '') {
+    errorValidation(email, 'Campo obrigatório!')
+  } else {
+    successValidation(email)
+  }
 
-    if (cpfValue === '') {
-        errorValidation(cpf, 'Campo obrigatório!')
-    } else if (cpfValue.length < 14) {
-        errorValidation(cpf, 'CPF Inválido!')
-    } else {
-        successValidation(cpf)
-    }
+  if (cpfValue === '') {
+    errorValidation(cpf, 'Campo obrigatório!')
+  } else if (cpfValue.length < 14) {
+    errorValidation(cpf, 'CPF Inválido!')
+  } else {
+    successValidation(cpf)
+  }
 
-    if (numbercvvValue === '') {
-        errorValidation(numbercvv, 'Campo obrigatório!')
-    } else if (numbercvvValue.length < 3) {
-        errorValidation(numbercvv, 'Padrão inválido para o CVV!')
-    } else {
-        successValidation(numbercvv)
-    }
+  if (numbercvvValue === '') {
+    errorValidation(numbercvv, 'Campo obrigatório!')
+  } else if (numbercvvValue.length < 3) {
+    errorValidation(numbercvv, 'Padrão inválido para o CVV!')
+  } else {
+    successValidation(numbercvv)
+  }
 
-    if (cardFlagValue === 'initial') {
-        errorValidation(cardFlag, 'Selecione uma bandeira!')
-    } else {
-        successValidation(cardFlag)
-    }
+  if (cardFlagValue === 'initial') {
+    errorValidation(cardFlag, 'Selecione uma bandeira!')
+  } else {
+    successValidation(cardFlag)
+  }
 
-    if (shelfLifeValue === '') {
-        errorValidation(shelfLife, 'Campo obrigatório!')
-    } else if (shelfLifeValue.length < 7) {
-        errorValidation(shelfLife, 'Informe o mês e o ano corretamente.')
-    } else {
-        successValidation(shelfLife)
-    }
+  if (shelfLifeValue === '') {
+    errorValidation(shelfLife, 'Campo obrigatório!')
+  } else if (shelfLifeValue.length < 7) {
+    errorValidation(shelfLife, 'Informe o mês e o ano corretamente.')
+  } else {
+    successValidation(shelfLife)
+  }
 
 }
 
 function errorValidation(input, message) {
-    const formControl = input.parentElement;
-    // console.log(formControl)
-    const small = formControl.querySelector('small')
+  const formControl = input.parentElement;
+  // console.log(formControl)
+  const small = formControl.querySelector('small')
 
-    small.innerText = message
+  small.innerText = message
 
-    formControl.className = 'input error'
+  formControl.className = 'input error'
 }
 
 function successValidation(input) {
-    const formControl = input.parentElement;
+  const formControl = input.parentElement;
 
-    formControl.className = 'input success'
+  formControl.className = 'input success'
 }
 
 function maskCpf() {
-    const masks = {
-        cpf(value) {
-            return value
-                .replace(/\D/g, "")
-                .replace(/(\d{3})(\d)/, "$1.$2")
-                .replace(/(\d{3})(\d)/, "$1.$2")
-                .replace(/(\d{3})(\d{1,2})/, "$1-$2")
-                .replace(/(-\d{2})\d+?$/, "$1");
-        },
-        shelfLife(value) {
-            return value
-                .replace(/\D/g, "")
-                .replace(/(\d{2})(\d)/, "$1/$2")
-        }
-    };
+  const masks = {
+    cpf(value) {
+      return value
+        .replace(/\D/g, "")
+        .replace(/(\d{3})(\d)/, "$1.$2")
+        .replace(/(\d{3})(\d)/, "$1.$2")
+        .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+        .replace(/(-\d{2})\d+?$/, "$1");
+    },
+    shelfLife(value) {
+      return value
+        .replace(/\D/g, "")
+        .replace(/(\d{2})(\d)/, "$1/$2")
+    }
+  };
 
-    document.getElementById('cpf').addEventListener(
-        "input",
-        e => {
-            e.target.value = masks['cpf'](e.target.value);
-        },
-        false
-    )
+  document.getElementById('cpf').addEventListener(
+    "input",
+    e => {
+      e.target.value = masks['cpf'](e.target.value);
+    },
+    false
+  )
 
-    document.getElementById('shelf-life').addEventListener(
-        "input",
-        e => {
-            e.target.value = masks['shelfLife'](e.target.value);
-        },
-        false
-    )
+  document.getElementById('shelf-life').addEventListener(
+    "input",
+    e => {
+      e.target.value = masks['shelfLife'](e.target.value);
+    },
+    false
+  )
 }
 
 maskCpf();
 
 
 function calculateTotal() {
-    const quantity = document.getElementById('quantity')
-    const price = document.getElementsByClassName('price')
-    let priceConvert = parseFloat(price[0].innerHTML.replace(',', '.').substring(2).trim())
-    let result = quantity.value * priceConvert
-    document.getElementById('totalPrice').value = `R$ ${result.toFixed(2).replace('.', ',')}`
+  const quantity = document.getElementById('quantity')
+  const price = document.getElementsByClassName('price')
+  let priceConvert = parseFloat(price[0].innerHTML.replace(',', '.').substring(2).trim())
+  let result = quantity.value * priceConvert
+  document.getElementById('totalPrice').value = `R$ ${result.toFixed(2).replace('.', ',')}`
 }
 
